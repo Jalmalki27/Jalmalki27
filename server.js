570cef2019f8160e00181069eaacc0088d284cbb
Jalmalki27 <91500751+Jalmalki27@users.noreply.github.com>
2021-12-19 20:18:07 +0300

    fiuish git and post

--- server.js
+++ server.js
@@ -3,13 +3,32 @@
 const db=require('./db')
 const Todo=require('./todo')
 //console.log(Todo)
+app.use(express.json())
  app.get('/',(req,res)=>{
      res.json('GET /IS WORKING')
 
  })
  app.get('/tasks',(req,res)=>{
-     res.json('GET /IS WORKING')
+    // res.json('GET /IS WORKING')
+    Todo.find({},(err,data)=>{
+        if(err){
+            console.log("ERROR:",err);
+        }else{
+            res.json(data);
+        }
+    })
  })
+ app.post("/tasks",(req,res)=>{
+     console.log('25:',req.err)
+     Todo.create(req.body,(err,newTask)=>{
+         if(err){
+             console.log("ERROR: ",err);
+  }else{
+      res.status(201).json(newTask);
+
+  }
+     });
+ });
 
  app.listen(5000,()=>{
      console.log('SERVER ID WORKING ...')
